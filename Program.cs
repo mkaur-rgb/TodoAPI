854377ef5a3ea489b689a7c558382291eae58182
mkaur-rgb <manpreetkaur6311062@gmail.com>
2025-02-07 22:09:44 -0500

    feat/New Todo API demo app for practice

--- Program.cs
+++ Program.cs
@@ -1,24 +1,50 @@
+using TodoAPI.AppDataContext;
+//using TodoAPI.Interface;
+using TodoAPI.Middleware;
+using TodoAPI.Models;
+//using TodoAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
 
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+// ....
+
+
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>(); // Add this line
+
+builder.Services.AddProblemDetails();  // Add this line
+
+// Adding of login 
+builder.Services.AddLogging();  //  Add this line
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+// ......
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); // Add this line
 
+app.UseExceptionHandler();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
